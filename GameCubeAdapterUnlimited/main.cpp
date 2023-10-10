97b684bbc7edbdfb3a6b4f29b4ecbfd62988c48c
SMarioMan <ultimatemariofan@yahoo.com>
2023-10-10 15:38:10 -0400

    Initial commit

---
+++ GameCubeAdapterUnlimited/main.cpp
@@ -0,0 +1,472 @@
+#include <windows.h>
+// Windows header must be defined before these to prevent build errors.
+#include <ViGEm/Client.h>
+#include <libusb/libusb.h>
+
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
+
+// Disabling this will save resources at run time but disables adapter
+// hotplugging.
+constexpr bool ENABLE_HOTPLUGGING = true;
+
+// The size of our adapters array.
+// A fixed-size array is used to ensure it can be accessed in a thread-safe way
+// without locks.
+constexpr std::size_t MAX_ADAPTERS = 24;
+
+static bool running = true;
+BOOL WINAPI CtrlHandler(DWORD event) {
+  if (event == CTRL_CLOSE_EVENT) {
+    running = false;
+    Sleep(20000);
+    return TRUE;
+  }
+  return FALSE;
+}
+
+struct Controller {
+#pragma pack(push, 1)
+  struct GCInput {
+    unsigned char On;
+    union {
+      unsigned short Buttons;
+      struct {
+        unsigned short A : 1;
+        unsigned short B : 1;
+        unsigned short X : 1;
+        unsigned short Y : 1;
+        unsigned short DpadLeft : 1;
+        unsigned short DpadRight : 1;
+        unsigned short DpadDown : 1;
+        unsigned short DpadUp : 1;
+        unsigned short Start : 1;
+        unsigned short Z : 1;
+        unsigned short R : 1;
+        unsigned short L : 1;
+      };
+    };
+    unsigned char AnalogX;
+    unsigned char AnalogY;
+    unsigned char CStickX;
+    unsigned char CStickY;
+    unsigned char LeftTrigger;
+    unsigned char RightTrigger;
+
+    GCInput()
+        : On(false),
+          Buttons(0),
+          AnalogX(128),
+          AnalogY(128),
+          CStickX(128),
+          CStickY(128),
+          LeftTrigger(0),
+          RightTrigger(0){};
+  };
+#pragma pack(pop)
+
+  static _DS4_REPORT GCtoDS4(const GCInput& gc) {
+    _DS4_REPORT ds4{};
+
+    ds4.bThumbLX = gc.AnalogX;
+    ds4.bThumbLY = -gc.AnalogY;
+    ds4.bThumbRX = gc.CStickX;
+    ds4.bThumbRY = -gc.CStickY;
+
+    ds4.wButtons = 0;
+    if (gc.Start) ds4.wButtons |= DS4_BUTTON_OPTIONS;
+    if (gc.Z) ds4.wButtons |= DS4_BUTTON_SHARE;
+    if (gc.R) ds4.wButtons |= DS4_BUTTON_SHOULDER_RIGHT;
+    if (gc.L) ds4.wButtons |= DS4_BUTTON_SHOULDER_LEFT;
+    if (gc.X) ds4.wButtons |= DS4_BUTTON_TRIANGLE;
+    if (gc.A) ds4.wButtons |= DS4_BUTTON_CIRCLE;
+    if (gc.B) ds4.wButtons |= DS4_BUTTON_CROSS;
+    if (gc.Y) ds4.wButtons |= DS4_BUTTON_SQUARE;
+
+    if (gc.DpadUp && gc.DpadLeft)
+      ds4.wButtons |= DS4_BUTTON_DPAD_NORTHWEST;
+    else if (gc.DpadDown && gc.DpadLeft)
+      ds4.wButtons |= DS4_BUTTON_DPAD_SOUTHWEST;
+    else if (gc.DpadDown && gc.DpadRight)
+      ds4.wButtons |= DS4_BUTTON_DPAD_SOUTHEAST;
+    else if (gc.DpadUp && gc.DpadRight)
+      ds4.wButtons |= DS4_BUTTON_DPAD_NORTHEAST;
+    else if (gc.DpadUp)
+      ds4.wButtons |= DS4_BUTTON_DPAD_NORTH;
+    else if (gc.DpadLeft)
+      ds4.wButtons |= DS4_BUTTON_DPAD_WEST;
+    else if (gc.DpadDown)
+      ds4.wButtons |= DS4_BUTTON_DPAD_SOUTH;
+    else if (gc.DpadRight)
+      ds4.wButtons |= DS4_BUTTON_DPAD_EAST;
+    else
+      ds4.wButtons |= DS4_BUTTON_DPAD_NONE;
+
+    ds4.bTriggerL = gc.LeftTrigger;
+    ds4.bTriggerR = gc.RightTrigger;
+
+    return ds4;
+  }
+};
+
+class Adapter {
+  static const unsigned char ReadEndpoint = 1 | LIBUSB_ENDPOINT_IN;
+  static const unsigned char WriteEndpoint = 2 | LIBUSB_ENDPOINT_OUT;
+
+  libusb_device_handle* dev_handle;
+
+  size_t failedReads = 0;
+
+  bool ReadInterrupt(unsigned char* data, const int& length,
+                     const int& timeoutMs) {
+    int actual;
+    const int interrupt = libusb_interrupt_transfer(
+        dev_handle, ReadEndpoint, data, length, &actual, timeoutMs);
+    if (interrupt < LIBUSB_SUCCESS) {
+      std::cout << "libusb_interrupt_transfer failed: " << interrupt
+                << std::endl;
+    }
+    return interrupt == LIBUSB_SUCCESS && length == actual;
+  }
+
+ public:
+  struct Inputs {
+    // Padding to align inputs we get from the USB transfer with the Controller
+    // struct.
+    unsigned char _pad[1]{};
+    Controller::GCInput Controllers[4]{};
+  };
+  Adapter(libusb_device_handle* dev_handle) {
+    this->dev_handle = dev_handle;
+    // This call makes Nyko-brand (and perhaps other) adapters work.
+    // However it returns LIBUSB_ERROR_PIPE with Mayflash adapters.
+    const int transfer = libusb_control_transfer(dev_handle, 0x21, 11, 0x0001,
+                                                 0, nullptr, 0, 1000);
+    if (transfer == LIBUSB_ERROR_PIPE) {
+      std::cout << "Mayflash adapter detected." << std::endl;
+    } else if (transfer < LIBUSB_SUCCESS) {
+      std::cout << "libusb_control_transfer failed: " << transfer << std::endl;
+    }
+    const int claim = libusb_claim_interface(dev_handle, 0);
+    if (claim < LIBUSB_SUCCESS) {
+      std::cout << "libusb_claim_interface failed: " << claim << std::endl;
+    }
+    // Initialization payload.
+    // Enable input polling by the adapter.
+    // Inputs can be read by the read endpoint.
+    unsigned char init = 0x13;
+    Write(&init, 1);
+    // Rumble payload.
+    // Disable rumble on all controllers.
+    // Likely nonessential.
+    unsigned char rumble[]{0x11, 0x0, 0x0, 0x0, 0x0};
+    Write(rumble, sizeof(rumble));
+  }
+  ~Adapter() {
+    const int release = libusb_release_interface(dev_handle, 0);
+    if (release < LIBUSB_SUCCESS) {
+      std::cout << "libusb_release_interface failed: " << release << std::endl;
+    }
+    libusb_close(dev_handle);
+  }
+  bool DoesHandleMatch(libusb_device_handle* dev_handle) {
+    return this->dev_handle == dev_handle;
+  }
+  bool DoesHandleMatch(Adapter* adapter) {
+    if (!adapter) {
+      std::cout << "Requested DoesHandleMatch on nullptr adapter" << std::endl;
+      return false;
+    }
+    return DoesHandleMatch(adapter->dev_handle);
+  }
+  bool Write(unsigned char* data, int length) {
+    int actual;
+    const int bulk = libusb_bulk_transfer(dev_handle, WriteEndpoint, data,
+                                          length, &actual, 0);
+    if (bulk < LIBUSB_SUCCESS) {
+      std::cout << "libusb_bulk_transfer failed: " << bulk << std::endl;
+    }
+    return bulk == LIBUSB_SUCCESS && length == actual;
+  }
+  bool GetInputs(Inputs& inputs) {
+    const int timeoutMs = 16;
+    return ReadInterrupt((unsigned char*)&inputs, sizeof(Inputs), timeoutMs);
+  }
+  // Detect timeouts due to multiple failed reads.
+  bool ShouldDisconnect(const bool& gotLastInput) {
+    if (gotLastInput) {
+      failedReads = 0;
+      return false;
+    }
+    if (failedReads++ > 20) {
+      return true;
+    }
+    return false;
+  }
+};
+
+// Globals.
+// The list of GameCube Adapters.
+std::array<std::atomic<Adapter*>, MAX_ADAPTERS> adapters;
+
+class ViGEmClient {
+  PVIGEM_CLIENT client;
+
+ public:
+  ViGEmClient() {
+    client = vigem_alloc();
+    if (!client) {
+      throw std::runtime_error("vigem_alloc failed");
+    }
+    const VIGEM_ERROR retval = vigem_connect(client);
+    if (!VIGEM_SUCCESS(retval)) {
+      std::stringstream ss;
+      ss << "vigem_connect failed with error code: 0x" << std::hex << retval
+         << std::endl;
+      throw std::runtime_error(ss.str());
+    }
+  }
+  ~ViGEmClient() {
+    vigem_disconnect(client);
+    vigem_free(client);
+  }
+  PVIGEM_TARGET AddController() {
+    // Allocate handle to identify new pad.
+    const PVIGEM_TARGET pad = vigem_target_ds4_alloc();
+    // Add client to the bus, this equals a plug-in event.
+    const VIGEM_ERROR pir = vigem_target_add(client, pad);
+    if (!VIGEM_SUCCESS(pir)) {
+      std::stringstream ss;
+      ss << "vigem_target_add failed with error code: 0x" << std::hex << pir
+         << std::endl;
+      throw std::runtime_error(ss.str());
+    }
+    return pad;
+  }
+  void RemoveController(PVIGEM_TARGET& pad) {
+    vigem_target_remove(client, pad);
+    vigem_target_free(pad);
+    pad = nullptr;
+  }
+  VIGEM_ERROR UpdateController(const PVIGEM_TARGET& pad,
+                               const DS4_REPORT& report) {
+    return vigem_target_ds4_update(client, pad, report);
+  }
+};
+
+class LibUSB {
+  // The vendor and product IDs associated with GameCube controller adapters.
+  // Any adapter placed in Wii U/Switch mode will appear with these IDs.
+  static const int VENDOR_ID = 0x57E;
+  static const int PRODUCT_ID = 0x337;
+
+  libusb_context* context = nullptr;
+
+ public:
+  LibUSB() { libusb_init(&context); }
+  ~LibUSB() {
+    if (context) {
+      libusb_exit(context);
+    }
+  }
+  void PollDevices() {
+    libusb_device** list;
+    // Hotplugging in Windows with libusb can only be done by getting the entire
+    // device list.
+    ssize_t num_devices = libusb_get_device_list(context, &list);
+    for (ssize_t i = 0; i < num_devices; i++) {
+      libusb_device* device = list[i];
+      libusb_device_descriptor desc;
+      int r = libusb_get_device_descriptor(device, &desc);
+      if (r < 0) {
+        continue;
+      }
+      if (desc.idVendor == VENDOR_ID && desc.idProduct == PRODUCT_ID) {
+        libusb_device_handle* dev_handle = nullptr;
+        // If we fail to open the device.
+        if (libusb_open(device, &dev_handle) != 0 || !dev_handle) {
+          continue;
+        }
+        Adapter* adapterPtr = new Adapter(dev_handle);
+        AddAdapter(adapterPtr);
+      }
+    }
+    libusb_free_device_list(list, 1);
+  }
+  // Assign an adapter to the lowest available index.
+  static void AddAdapter(Adapter* adapterPtr) {
+    size_t idx = -1;
+    // Find the next free spot to load an adapter into.
+    const auto it = std::find(adapters.begin(), adapters.end(), nullptr);
+    if (it != adapters.end()) {
+      *it = adapterPtr;
+      idx = std::distance(adapters.begin(), it);
+      std::cout << "Adapter " << idx << " connected." << std::endl;
+    }
+    // No spot found.
+    else {
+      throw std::runtime_error(
+          "No free spots left in adapters array. Please increase MAX_ADAPTERS "
+          "and recompile.");
+    }
+    return;
+  }
+  static void RemoveAdapter(Adapter* adapterPtr) {
+    // Find the associated adapter and deallocate it.
+    for (std::atomic<Adapter*>& adapter : adapters) {
+      if (!adapter.load()) {
+        continue;
+      }
+      if (adapter.load()->DoesHandleMatch(adapterPtr)) {
+        auto tmp = adapter.load();
+        adapter.store(nullptr);
+        delete tmp;
+        return;
+      }
+    }
+    std::cout << "Could not find target adapter for removal." << std::endl;
+  }
+  static size_t NumAdapters() {
+    size_t count = 0;
+    for (const Adapter* adapter : adapters) {
+      if (adapter) {
+        count++;
+      }
+    }
+    return count;
+  }
+};
+
+class AdapterThread {
+ public:
+  AdapterThread() { SetupPads(); }
+
+ private:
+  void SetupPads() {
+    // Set up the virtual gamepads.
+    while (pads.size() / 4 < adapters.size()) {
+      for (size_t i = 0; i < 4; i++) {
+        pads.push_back(vigemClient.AddController());
+        // Initialize the inputs to nothing.
+        const Controller::GCInput resetGCInput;
+        const DS4_REPORT report = Controller::GCtoDS4(resetGCInput);
+        vigemClient.UpdateController(pads.back(), report);
+        // Initialize as disconnected, since we do not yet know if a controller
+        // is there.
+        isConnected.push_back(false);
+      }
+    }
+  }
+
+ public:
+  void run() {
+    while (running) {
+      // Allocate new virtual pads as needed.
+      SetupPads();
+      // Read inputs and update virtual gamepads.
+      for (size_t i = 0; i < adapters.size(); i++) {
+        // Missing adapters are skipped.
+        if (!adapters[i]) {
+          continue;
+        }
+        Adapter::Inputs inputs;
+        // If we fail to get the latest inputs, remove the lost adapter.
+        // TODO: Find a better way to detect disconnections.
+        const bool gotLastInput = adapters[i].load()->GetInputs(inputs);
+        if (adapters[i].load()->ShouldDisconnect(gotLastInput)) {
+          LibUSB::RemoveAdapter(adapters[i]);
+          std::cout << "Adapter " << i << " disconnected" << std::endl;
+          // Associated pads are marked as disconnected.
+          // NOTE: This assumes inputs.Controllers[j].On remains true.
+          for (size_t j = 0; j < 4; j++) {
+            size_t index = i * 4 + j;
+            isConnected[index] = false;
+          }
+        }
+        // Do not update the virtual gamepads if the adapter failed to report
+        // new inputs.
+        if (!gotLastInput) {
+          continue;
+        }
+        // Update the inputs of each virtual gamepad.
+        for (size_t j = 0; j < 4; j++) {
+          const size_t index = i * 4 + j;
+          // Check for a connection change.
+          if (isConnected[index] != (bool)inputs.Controllers[j].On) {
+            isConnected[index] = (bool)inputs.Controllers[j].On;
+            if (isConnected[index]) {
+              std::cout << "Controller " << index << " connected" << std::endl;
+            } else {
+              // Disconnected controllers are reset.
+              const Controller::GCInput resetGCInput;
+              const DS4_REPORT report = Controller::GCtoDS4(resetGCInput);
+              vigemClient.UpdateController(pads[index], report);
+              std::cout << "Controller " << index << " disconnected"
+                        << std::endl;
+            }
+          }
+          if (!inputs.Controllers[j].On) {
+            continue;
+          }
+          if (pads.size() < index) {
+            throw std::out_of_range(
+                "Not enough virtual pads allocated to handle adapter inputs.");
+          }
+          DS4_REPORT report = Controller::GCtoDS4(inputs.Controllers[j]);
+          vigemClient.UpdateController(pads[index], report);
+          // DEBUG
+          /*if (inputs.Controllers[j].A) {
+                          std::cout << "Detected A press on controller " <<
+          index << std::endl;
+          }*/
+        }
+      }
+    }
+    // Tear down gamepads when the loop is over.
+    for (PVIGEM_TARGET& pad : pads) {
+      vigemClient.RemoveController(pad);
+    }
+  }
+  // The list of virtual gamepads.
+  std::vector<PVIGEM_TARGET> pads;
+  // The ViGEmClient. Shared between adapters.
+  ViGEmClient vigemClient = ViGEmClient();
+  // The controller connection state from the previous loop.
+  // Used to detect connection status changes.
+  // Corresponds directly to the pads vector.
+  std::vector<bool> isConnected;
+};
+
+int main(int argc, char* argv[]) {
+  std::cout << "Input feeder started" << std::endl;
+
+  // Set a handler to gracefully close on Ctrl+C.
+  SetConsoleCtrlHandler(CtrlHandler, TRUE);
+
+  LibUSB libUsb = LibUSB();
+
+  // Start the adapter thread to update inputs.
+  // Multithreading ensures that polling for new adapters doesn't stall input
+  // updates.
+  AdapterThread adapterThread = AdapterThread();
+  std::thread thread(&AdapterThread::run, &adapterThread);
+
+  do {
+    // Poll for new adapter connections.
+    libUsb.PollDevices();
+    // Only check for new controllers at a fixed interval.
+    // This prevents busy polling from maxing out a thread.
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+  } while (running && ENABLE_HOTPLUGGING);
+
+  thread.join();
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# gamecube-adapter-unlimited
+# GameCube Adapter Unlimited
+
 GameCube Adapter Feeder via ViGEm, with support for unlimited adapters
+
+This feeder uses ViGEm to present GameCube controllers as DualShock 4 controllers.
+Unlike the alternatives, which are usually limited to a single adapter, this tool supports unlimited adapters, or at least up to the USB spec limit.
+This is particularly useful for massively multiplayer sessions, such as 24-player Mario Kart Wii splitscreen:
+
+[![Insane 24-Player Mario Kart Wii Split Screen Test!
+](https://img.youtube.com/vi/LgkHVOwAPvo/0.jpg)](https://youtu.be/LgkHVOwAPvo)
+
+## Features
+* An unlimited number of controllers and adapters can be used at once, for local massively multiplayer sessions.
+* Hotpluggable adapters and controllers: Adapters and controllers can be freely attached and detached without restarting the application. Ports maintain the same controller assignment and adapters fill in missing slots.
+
+## Libraries Used
+* libusb
+* ViGEm
+
+## Build (optional)
+1. Download and install Visual Studio 2022.
+1. Clone the repository.
+1. Initialize submodules: `git submodule update --init --recursive`.
+1. Open in Visual Studio and build the project.
+
+## Install
+1. Install ViGEm Driver https://github.com/ViGEm/ViGEmBus/releases/
+1. Install the WinUSB driver using Zadig: https://dolphin-emu.org/docs/guides/how-use-official-gc-controller-adapter-wii-u/#Using_Zadig
+1. Download and extract the feeder: https://github.com/SMarioMan/gamecube-adapter-unlimited/releases
+
+## Run
+1. Open `GameCubeAdapterUnlimited.exe`.
+1. On launch, the program prints `Input feeder started`.
+1. Attach adapters in the desired port order. The feeder notifies you when an adapter is connected.
+1. If a controller or adapter is disconnected, you can reattach it and port assignments should be preserved, no restart required.
